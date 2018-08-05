a24509ebbaeb27865cf3d4eac64a324f801d8aca
xiyangyangpan <39935760+xiyangyangpan@users.noreply.github.com>
2018-08-05 16:29:30 +0800

    Update my_module.js

--- app/modules/custom/my_module/my_module.js
+++ app/modules/custom/my_module/my_module.js
@@ -1,1 +1,26 @@
-test
+/**
+ * Implements hook_menu().
+ */
+function my_module_menu() {
+  var items = {};
+  items['hello_world'] = {
+    title: 'DrupalGap',
+    page_callback: 'my_module_hello_world_page'
+  };
+  return items;
+}
+
+/**
+ * The callback for the "Hello World" page.
+ */
+function my_module_hello_world_page() {
+  var content = {};
+  content['my_button'] = {
+    theme: 'button',
+    text: 'Hello World',
+    attributes: {
+      onclick: "drupalgap_alert('Hi!')"
+    }
+  };
+  return content;
+}
